695a54771c024b14548fd642be1d5cf96521df28
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-02 12:09:20 +0000

    implement add item button functionality (dummy data)

--- src/components/shopping-list-form.js
+++ src/components/shopping-list-form.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addListItem } from '../actions';
 import ShoppingListEntry from './shopping-list-entry';
 import ShoppingListSubmit from './shopping-list-submit';
 import ShoppingListLabel from './shopping-list-label';
 
-export default function ShoppingListForm() {
-  return (
-    <form id="js-shopping-list-form">
-      <ShoppingListLabel />
-      <ShoppingListEntry />
-      <ShoppingListSubmit />
-    </form>
-  );
+export class ShoppingListForm extends React.Component {
+  addListItem(name) {
+    this.props.dispatch(addListItem(name));
+  }
+  
+  render() {
+    const name = 'ice';
+    return (
+      <form id="js-shopping-list-form">
+        <ShoppingListLabel />
+        <ShoppingListEntry />
+        <ShoppingListSubmit addListItem={() => this.addListItem(name)}/>
+      </form>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  shoppingList: state.shoppingList
+})
+
+export default connect(mapStateToProps)(ShoppingListForm)
--- src/components/shopping-list-submit.js
+++ src/components/shopping-list-submit.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default function ShoppingListSubmit() {
-  return <button type="submit">Add item</button>;
+export default function ShoppingListSubmit(props) {
+  return (
+    <button type="submit" 
+      onClick={(e) => {
+        e.preventDefault();
+        props.addListItem();
+      }}
+      >Add item</button>
+  )
 }
